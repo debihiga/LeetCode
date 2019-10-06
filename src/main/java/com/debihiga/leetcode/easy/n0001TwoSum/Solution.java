fd631746de5fc4636802e929a4f7fd7d1e84c92d
debihiga <debihiga@gmail.com>
2019-10-06 04:28:27 -0300

    Add: 1 Two Sum

---
+++ src/main/java/com/debihiga/leetcode/easy/n0001TwoSum/Solution.java
@@ -0,0 +1,5 @@
+package com.debihiga.leetcode.easy.n0001TwoSum;
+
+public interface Solution {
+    public int[] twoSum(int[] nums, int target);
+}
---
+++ src/test/java/com/debihiga/leetcode/easy/n0001TwoSum/TwoSumBase.java
@@ -0,0 +1,20 @@
+package com.debihiga.leetcode.easy.n0001TwoSum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class TwoSumBase {
+
+    Solution solution;
+
+    public void test1() {
+        int [] result = solution.twoSum(new int[] {2, 7, 11, 15}, 9);
+        assertArrayEquals(new int[] {0,1}, result);
+    }
+
+    public void test2() {
+        int [] result = solution.twoSum(new int[] {0,4,3,0}, 0);
+        assertArrayEquals(new int[] {0,3}, result);
+    }
+}
